d0335d39c3b380e2f12a2bb929e82acd90fa2243
vopisk <vopisk@877af551-a721-0410-b320-bfceb2facb6f>
2007-10-30 20:43:33 +0000

    Check out MudObject_new.py for the rewrite of the MudObject.py file.  The relevant info for applying a template is not in yet, I still want to see about using templates to give objects additional functions that can be called like normal functions.  This will require further testing.  The changes to logger.py are for doing system traceback logging. :D

--- branches/XML_Templates/logger.py
+++ branches/XML_Templates/logger.py
@@ -1,9 +1,34 @@
 import logging
 import time
 import MudConst
+import sys
+import traceback
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filename=MudConst.log_dir+str(time.strftime("%a %d %b %Y", time.gmtime()))+'.txt',
                     filemode='w')
+
+
+def logTraceback():
+    """
+    A function to write a traceback to the log.
+    For now, we'll also print the exception to
+    the screen as well.
+    """
+    logging.error("Error in execution: " + str(sys.exc_info()[0]) + "\r\n" + listf(format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback)))
+    print "Error in execution: " + str(sys.exc_info()[0]) + "\r\n" + listf(format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback))
+
+
+def listf(data):
+    """
+    This function will format the output of a traceback.
+    I'm putting it here, because it seems fitting and I
+    don't want to have to redefine the behavior everywhere
+    we want to use tracebacks to find out what's going on.
+    """
+    buffer = ""
+    for line in data:
+        buffer = buffer + line + "\n"
+    return buffer
